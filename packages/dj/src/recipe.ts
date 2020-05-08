1546e99ef42e29c3114108f3165e6b7b063bc3a9
clearloop <udtrokia@gmail.com>
2020-05-09 01:18:01 +0800

    docs: config in workspace, usages in dj

--- packages/dj/src/recipe.ts
+++ packages/dj/src/recipe.ts
@@ -1,8 +1,79 @@
 import yargs from "yargs";
-import { autoAPI, ShadowAPI } from "@darwinia/api";
+import { autoAPI, API, ShadowAPI } from "@darwinia/api";
 import { Vec, Struct } from "@polkadot/types";
 import { Config, IDarwiniaEthBlock, log } from "@darwinia/util";
 
+/**
+ * Helpers
+ */
+async function recipeBalance(api: API, args: yargs.Arguments) {
+    let addr = (args.address as string);
+    if (addr === "") {
+        addr = api.account.address;
+    }
+
+    const balance = await api.getBalance(addr);
+    const s = balance + " RING 💰";
+    log.ox(s);
+}
+
+async function recipeBestHeader(api: API, shadow: ShadowAPI) {
+    const bestHeaderHash = await api._.query.ethRelay.bestHeaderHash();
+    const last = await shadow.getBlock(bestHeaderHash.toString());
+    log.ox(JSON.stringify(last, null, 2));
+}
+
+async function recipeHeader(api: API, args: yargs.Arguments, shadow: ShadowAPI) {
+    const block = (args.block as string);
+    let header: IDarwiniaEthBlock | null = null;
+    if (block === "") {
+        const hash = await api._.query.ethRelay.bestHeaderHash();
+        header = await shadow.getBlock(hash.toString());
+    }
+
+    header = await shadow.getBlock(block);
+    log.ox(JSON.stringify(header, null, 2));
+}
+
+async function recipeCodec(api: API, args: yargs.Arguments, shadow: ShadowAPI) {
+    const codecBlock: string = (args.block as string);
+    let pair: any = null;
+    if (codecBlock === "") {
+        const hash = await api._.query.ethRelay.bestHeaderHash();
+        pair = await shadow.getBlockWithProof(hash.toString());
+    }
+
+    // Get proof by hash or number
+    if (pair === null) {
+        if (codecBlock.length >= 64) {
+            pair = await shadow.getBlockWithProof(codecBlock);
+        } else {
+            pair = await shadow.getBlockWithProof(Number.parseInt(codecBlock, 10));
+        }
+    }
+
+    log.trace(pair);
+    // codec resp
+    const resp = {
+        eth_header: new Struct(
+            api._.registry,
+            api.types.EthHeader,
+            pair[0]
+        ).toHex(),
+        proof: new Vec(
+            api._.registry,
+            (api._.registry.get("DoubleNodeWithMerkleProof") as any),
+            pair[1],
+        ).toHex(),
+    }
+
+    // log proofs
+    log.ox(JSON.stringify(resp, null, 2));
+}
+
+/**
+ * Command Recipe
+ */
 const cmdRecipe: yargs.CommandModule = {
     builder: (argv: yargs.Argv) => {
         return argv.positional("recipe", {
@@ -25,7 +96,7 @@ const cmdRecipe: yargs.CommandModule = {
         });
     },
     command: "info <recipe>",
-    describe: "Get info of some recipes",
+    describe: "Get info of recipes",
     handler: async (args: yargs.Arguments) => {
         const api = await autoAPI();
         const cfg = new Config();
@@ -33,65 +104,16 @@ const cmdRecipe: yargs.CommandModule = {
 
         switch (args.recipe) {
             case "balance":
-                let addr = (args.address as string);
-                if (addr === "") {
-                    addr = api.account.address;
-                }
-
-                const balance = await api.getBalance(addr);
-                const s = balance + " RING 💰";
-                log.ox(s);
+                await recipeBalance(api, args);
                 break;
             case "bestHeader":
-                const bestHeaderHash = await api._.query.ethRelay.bestHeaderHash();
-                const last = await shadow.getBlock(bestHeaderHash.toString());
-                log.ox(JSON.stringify(last, null, 2));
+                await recipeBestHeader(api, shadow);
                 break;
             case "header":
-                const block = (args.block as string);
-                let header: IDarwiniaEthBlock | null = null;
-                if (block === "") {
-                    const hash = await api._.query.ethRelay.bestHeaderHash();
-                    header = await shadow.getBlock(hash.toString());
-                }
-
-                header = await shadow.getBlock(block);
-                log.ox(JSON.stringify(header, null, 2));
+                await recipeHeader(api, args, shadow);
                 break;
             case "codec":
-                const codecBlock: string = (args.block as string);
-                let pair: any = null;
-                if (codecBlock === "") {
-                    const hash = await api._.query.ethRelay.bestHeaderHash();
-                    pair = await shadow.getBlockWithProof(hash.toString());
-                }
-
-                // Get proof by hash or number
-                if (pair === null) {
-                    if (codecBlock.length >= 64) {
-                        pair = await shadow.getBlockWithProof(codecBlock);
-                    } else {
-                        pair = await shadow.getBlockWithProof(Number.parseInt(codecBlock, 10));
-                    }
-                }
-
-                log.trace(pair);
-                // codec resp
-                const resp = {
-                    eth_header: new Struct(
-                        api._.registry,
-                        api.types.EthHeader,
-                        pair[0]
-                    ).toHex(),
-                    proof: new Vec(
-                        api._.registry,
-                        (api._.registry.get("DoubleNodeWithMerkleProof") as any),
-                        pair[1],
-                    ).toHex(),
-                }
-
-                // log proofs
-                log.ox(JSON.stringify(resp, null, 2))
+                await recipeCodec(api, args, shadow);
                 break;
             default:
                 break;
