a4de9867dac92540314519908583ca03f5c823ed
clearloop <udtrokia@gmail.com>
2020-08-17 20:44:47 +0800

    chore(bump): bump versions

--- packages/mock/honest.ts
+++
@@ -1,8 +0,0 @@
-import { log } from "../util";
-import { proposal } from "../dj/src/proposal";
-
-/// The normal proposal submitter
-(async () => {
-    log.event("This is the honest relayer");
-    await proposal(19);
-})();
