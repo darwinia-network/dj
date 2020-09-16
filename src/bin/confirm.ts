7de4565fbcea241b2c7bdaef15e356372d90367d
clearloop <udtrokia@gmail.com>
2020-09-16 12:03:14 +0800

    feat(cmd): add redeem debug tool

--- src/bin/confirm.ts
+++ src/bin/confirm.ts
@@ -5,7 +5,7 @@ import { API, ShadowAPI } from "../api";
 (async () => {
     const args = process.argv.slice(2);
     if (args.length !== 1) {
-        log.warn("Usage: dj-confirm <number>");
+        console.log("Usage: dj-confirm <number>");
         return;
     }
 
---
+++ src/bin/verify.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import { Config, log } from "../util";
+import { API, ShadowAPI } from "../api";
+
+(async () => {
+    const args = process.argv.slice(2);
+    if (args.length !== 1) {
+        console.log("Usage:   dj-verify <tx>/<confirmed> [-d deposit]");
+        console.log("Example: dj-verify 0x1d3ef601b9fa4a7f1d6259c658d0a10c77940fa5db9e10ab55397eb0ce88807d/8694126");
+        console.log("Example: dj-verify 0x1d3ef601b9fa4a7f1d6259c658d0a10c77940fa5db9e10ab55397eb0ce88807d/8694126 -d");
+        return;
+    }
+
+    /// Init logs
+    process.env.LOGGER = "ALL";
+
+    /// Init API
+    const conf = new Config();
+    const api = await API.auto();
+    const shadow = new ShadowAPI(conf.shadow);
+    const target = args[0].slice("/");
+
+    // Trigger relay
+    try {
+        await api.redeem(
+            args.contains("-d")? "Deposit": "Token",
+            await shadow.getReceipt(target[0], target[1])
+        );
+        log.ox(`Redeem tx ${target} succeed!`);
+    } catch (e) {
+        log.ex(e);
+    }
+})();
